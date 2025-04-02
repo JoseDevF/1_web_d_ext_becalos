995981c9856ed31010b60dca083eb50f40ff6db6
José Hernández <carrousel.182@hotmail.com>
2025-04-01 19:22:08 -0600

    Ejercicio recursivo

---
+++ Clase_05/main.js
@@ -0,0 +1,41 @@
+function timer(n) {
+    if (n === 0) {
+        console.log("¡Despegue!");
+        return;
+    }
+    setTimeout(() => {
+        timer(n - 1);
+    }, 1000)
+    console.log(n);
+}
+
+console.log(timer(5));
+
+function sum(n) {
+    if (n === 10) return 0;
+    return n + sum(n + 1)
+}
+
+console.log(sum(5));
+
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log("Factorial: ", factorial(5));
+
+
+const arrayN = [1,
+    [2, 3],
+    [4,
+        [5],
+        [6,
+            [10]
+        ]
+    ]
+];
+
+function sumArr(arr) {
+
+}
